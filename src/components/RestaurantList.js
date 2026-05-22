9b25b9e9c7b2d126f32d7256a674b7362588c2c3
Khushbu Hasija <hasijakhushbu@gmail.com>
2026-05-22 14:43:07 +0530

    Do not include corrupted images

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -34,21 +34,21 @@ function RestaurantList() {
   return (
     <div className="res-container">
       {restaurants.map((restaurant) => {
-        console.log(RESTAURANT_LIST_IMAGES_URL);
-        return (
-          <div className="res-card" key={restaurant.info.id}>
-            <img
-              className="res-image"
-              alt="res-image"
-              src={
-                RESTAURANT_LIST_IMAGES_URL + restaurant.info.cloudinaryImageId
-              }
-            />
-            <h3>{restaurant.info.name}</h3>
-            <p>{restaurant.info.cuisines.join(", ")}</p>
-            <p>Rating: {restaurant.info.avgRatingString}</p>
-          </div>
-        );
+        if (restaurant.info.cloudinaryImageId.startsWith("RX"))
+          return (
+            <div className="res-card" key={restaurant.info.id}>
+              <img
+                className="res-image"
+                alt="res-image"
+                src={
+                  RESTAURANT_LIST_IMAGES_URL + restaurant.info.cloudinaryImageId
+                }
+              />
+              <h3>{restaurant.info.name}</h3>
+              <p>{restaurant.info.cuisines.join(", ")}</p>
+              <p>Rating: {restaurant.info.avgRatingString}</p>
+            </div>
+          );
       })}
     </div>
   );
