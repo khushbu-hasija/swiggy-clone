696520a427e9db81a3e28ce57761d3dfc27685c3
Khushbu Hasija <hasijakhushbu@gmail.com>
2026-05-24 13:24:36 +0530

    Added links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import logo from "url:../../assets/logo.jpg";
-
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="header">
@@ -8,9 +8,15 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
           <li>Cart</li>
         </ul>
       </div>
